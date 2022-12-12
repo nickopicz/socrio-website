50d1bd298fe9dd362dbafa1d4ebea0e8c3ba668d
Nick Ciraulo <nickopicz@gmail.com>
2022-12-12 00:10:27 -0500

    added landing page

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { LandingPage } from "./screens/LandingScreen";
+import { useFonts } from "expo-font";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,16 +1,123 @@
 import React from "react";
-import { SafeAreaView, Text, ImageBackground } from "react-native";
-import { Dim } from "../Constants";
+import { View, Text, Image } from "react-native";
+import { Colors, Dim } from "../Constants";
+import { useFonts } from "expo-font";
+import * as Linking from "expo-linking";
+import { LinearGradient } from "expo-linear-gradient";
+
+import CustomText from "../components/common/Text";
+import { RoundedButton } from "../components/common/Button";
 
 export const LandingPage = ({ navigation }) => {
+  function handlePress() {
+    Linking.openURL(
+      "https://docs.google.com/forms/d/e/1FAIpQLSe4Szo17_R3TWXCnpAtdYN2cnGpE_t4gCjgpWluKWBbX1UeMw/viewform?usp=sf_link"
+    );
+  }
   return (
-    <SafeAreaView>
-      <ImageBackground
-        source={require("../assets/stars.gif")}
-        resizeMode="repeat"
-        style={{ height: Dim.height, width: Dim.width }}
-      />
-      <Text>Hello World</Text>
-    </SafeAreaView>
+    <View
+      style={{
+        width: Dim.width,
+        height: Dim.height,
+        alignItems: "center",
+        flexDirection: "row",
+      }}
+    >
+      <LinearGradient
+        colors={[Colors.aegean, Colors.black]}
+        style={{
+          width: Dim.width * 0.25,
+          height: Dim.height,
+        }}
+        start={{ x: 0, y: 1 }}
+      >
+        <View></View>
+      </LinearGradient>
+
+      <View
+        style={{
+          alignItems: "center",
+          width: Dim.width * 0.5,
+          height: Dim.height,
+          backgroundColor: Colors.black,
+        }}
+      >
+        <View style={{ alignItems: "center", marginTop: 40 }}>
+          <Image
+            source={require("../assets/ancient_agora.jpg")}
+            resizeMethod="auto"
+            style={{
+              height: Dim.height * 0.25,
+              width: Dim.width * 0.5,
+              opacity: 0.5,
+              borderRadius: 20,
+            }}
+          />
+          <CustomText
+            aegean
+            style={{
+              fontSize: 90,
+              position: "absolute",
+              opacity: 0.8,
+              shadowColor: Colors.white,
+              shadowOffset: {
+                width: 0,
+                height: 10,
+              },
+              shadowOpacity: 0.2,
+            }}
+          >
+            socrio
+          </CustomText>
+        </View>
+        <View style={{ padding: 200 }}>
+          <RoundedButton
+            small
+            style={{
+              height: Dim.height / 9,
+              width: Dim.width * 0.25,
+              shadowColor: Colors.white,
+              shadowOffset: {
+                width: 0,
+                height: 7,
+              },
+              shadowOpacity: 0.1,
+            }}
+            onPress={() => handlePress()}
+          >
+            <CustomText black h2>
+              Join the Info List!
+            </CustomText>
+          </RoundedButton>
+        </View>
+        <Image
+          source={require("../assets/app_store_apple.jpg")}
+          resizeMethod="auto"
+          style={{
+            height: Dim.height * 0.075,
+            width: Dim.width * 0.13,
+            opacity: 0.8,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: Colors.powder,
+            marginTop: -10,
+            shadowColor: Colors.white,
+            shadowOffset: {
+              width: 0,
+              height: 10,
+            },
+            shadowOpacity: 0.2,
+          }}
+        />
+      </View>
+      <LinearGradient
+        style={{
+          width: Dim.width * 0.25,
+          height: Dim.height,
+        }}
+        colors={[Colors.aegean, Colors.black]}
+        start={{ x: 1, y: 1 }}
+      ></LinearGradient>
+    </View>
   );
 };
