95333dee24e33637c819c528ecd794affa1383d8
Nick Ciraulo <nickopicz@gmail.com>
2022-12-12 20:27:11 -0500

    styles: changed the layout to be friendly towards mobile browsers.

---
+++ components/Card.js
@@ -0,0 +1,51 @@
+/**
+ *
+ *   |\\  ||    ||\\\\
+ *   ||\\ ||    ||
+ *   || \\||    ||
+ *   ||  \||    ||////
+ *
+ *   ------This file was created, authored, currently owned and all rights belong to the author NC------
+ *
+ *                                  Author: Nicholas Ciraulo
+ */
+import React from "react";
+import { View, StyleSheet, ImageProps } from "react-native";
+import Text from "./common/Text";
+import { Card } from "react-native-elements";
+import { Colors, Dim } from "../Constants";
+
+/**
+ * @props title = the title of the card
+ * @props imageSource = source of the media
+ * @props textContent = inner paragraph style text
+ * @returns card component
+ */
+export const CardHolder = ({ title, imageSource, textContent }) => {
+  return (
+    <Card containerStyle={styles.border}>
+      <Card.Title>
+        <Text h4 u led>
+          {title}
+        </Text>
+      </Card.Title>
+      <Card.Divider />
+      {imageSource && <Card.Image source={imageSource} />}
+      <Text p1 led style={{ marginBottom: 10, textAlign: "center" }}>
+        {textContent}
+      </Text>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  border: {
+    backgroundColor: Colors.night,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.night,
+    alignItems: "center",
+    opacity: 0.8,
+    width: Dim.width * 0.5,
+  },
+});
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useRef } from "react";
-import { View, Text, Image, ScrollView } from "react-native";
+import { View, Text, Image, ScrollView, ImageBackground } from "react-native";
 import { Colors, Dim } from "../Constants";
 import { useFonts } from "expo-font";
 import * as Linking from "expo-linking";
 import { LinearGradient } from "expo-linear-gradient";
-import Lottie from "lottie-react-native";
+import LottieView from "lottie-react-native";
 import CustomText from "../components/common/Text";
 import { RoundedButton } from "../components/common/Button";
+import { CardHolder } from "../components/Card";
 
 export const LandingPage = ({ navigation }) => {
   const scrollAnim = useRef(null);
@@ -21,110 +22,154 @@ export const LandingPage = ({ navigation }) => {
     );
   }
   return (
-    <View
-      style={{
-        height: Dim.height,
-        width: Dim.height,
-        alignItems: "center",
-        flexDirection: "row",
-      }}
-    >
-      <LinearGradient
-        colors={[Colors.aegean, Colors.black]}
-        style={{
-          width: Dim.width * 0.25,
-          height: Dim.height,
-        }}
-        start={{ x: 0, y: 1 }}
+    <View style={{ height: Dim.height * 2, width: Dim.width }}>
+      <ImageBackground
+        source={require("../assets/stars.gif")}
+        resizeMode="repeat"
+        style={{ height: Dim.height * 2, width: Dim.width }}
       >
-        <View></View>
-      </LinearGradient>
-
-      <View
-        style={{
-          alignItems: "center",
-          width: Dim.width * 0.5,
-          height: Dim.height,
-          backgroundColor: Colors.black,
-        }}
-      >
-        <View style={{ alignItems: "center", marginTop: 40 }}>
-          <Image
-            source={require("../assets/ancient_agora.jpg")}
-            resizeMethod="auto"
-            style={{
-              height: Dim.height * 0.25,
-              width: Dim.width * 0.5,
-              opacity: 0.5,
-              borderRadius: 20,
-            }}
-          />
-          <CustomText
-            aegean
-            style={{
-              fontSize: 90,
-              position: "absolute",
-              opacity: 0.8,
-              shadowColor: Colors.white,
-              shadowOffset: {
-                width: 0,
-                height: 10,
-              },
-              shadowOpacity: 0.2,
-            }}
-          >
-            socrio
-          </CustomText>
-        </View>
-        <View style={{ padding: 200 }}>
-          <RoundedButton
-            small
-            style={{
-              height: Dim.height / 9,
-              width: Dim.width * 0.25,
-              shadowColor: Colors.white,
-              shadowOffset: {
-                width: 0,
-                height: 7,
-              },
-              shadowOpacity: 0.1,
-            }}
-            onPress={() => handlePress()}
-          >
-            <CustomText black h2>
-              Join the Info List!
-            </CustomText>
-          </RoundedButton>
-        </View>
-
-        <Image
-          source={require("../assets/app_store_apple.jpg")}
-          resizeMethod="auto"
+        <View
           style={{
-            height: Dim.height * 0.075,
-            width: Dim.width * 0.13,
-            opacity: 0.8,
-            borderRadius: 10,
-            borderWidth: 1,
-            borderColor: Colors.powder,
-            marginTop: -10,
-            shadowColor: Colors.white,
-            shadowOffset: {
-              width: 0,
-              height: 10,
-            },
-            shadowOpacity: 0.2,
+            height: Dim.height * 2,
+            width: Dim.width,
+            alignItems: "center",
+            flexDirection: "row",
           }}
-        />
-      </View>
-      <LinearGradient
-        style={{
-          width: Dim.width * 0.25,
-          height: Dim.height,
-        }}
-        colors={[Colors.aegean, Colors.black]}
-        start={{ x: 1, y: 1 }}
-      ></LinearGradient>
+        >
+          <LinearGradient
+            colors={[Colors.aegean, Colors.black]}
+            style={{
+              width: Dim.width * 0.25,
+              height: Dim.height * 2,
+              opacity: 0.7,
+            }}
+            start={{ x: 0, y: 1 }}
+          ></LinearGradient>
+
+          <View
+            style={{
+              alignItems: "center",
+              width: Dim.width * 0.5,
+              height: Dim.height * 2,
+              backgroundColor: Colors.black,
+              opacity: 1,
+            }}
+          >
+            <View
+              style={{
+                padding: 20,
+              }}
+            >
+              <Image
+                source={require("../assets/socrio_large_logo.jpg")}
+                resizeMethod="auto"
+                style={{
+                  height: Dim.height * 0.25,
+                  width: Dim.width * 0.5,
+                  opacity: 1,
+                  borderRadius: 20,
+                }}
+              />
+            </View>
+            <View
+              style={{
+                paddingTop: Dim.height * 0.2,
+                paddingBottom: Dim.height * 0.3,
+              }}
+            >
+              <RoundedButton
+                small
+                style={{
+                  height: Dim.height * 0.075,
+                  width: Dim.width * 0.125,
+                  shadowColor: Colors.white,
+                  shadowOffset: {
+                    width: 0,
+                    height: 7,
+                  },
+                  shadowOpacity: 0.1,
+                }}
+                onPress={() => handlePress()}
+              >
+                <CustomText black h4 style={{ position: "relative" }}>
+                  join
+                </CustomText>
+              </RoundedButton>
+            </View>
+            {/* <View
+              style={{ height: 50, width: 160, backgroundColor: Colors.aegean }}
+            > */}
+            <Image
+              source={require("../assets/app_store_apple.jpg")}
+              resizeMethod="auto"
+              style={{
+                height: 55,
+                width: 180,
+                opacity: 0.8,
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: Colors.powder,
+                shadowColor: Colors.white,
+                shadowOffset: {
+                  width: 0,
+                  height: 10,
+                },
+                shadowOpacity: 0.2,
+              }}
+            />
+            {/* </View> */}
+            <View
+              style={{ alignItems: "center", paddingTop: Dim.height * 0.4 }}
+            >
+              <CardHolder
+                title={"Have your voice heard"}
+                textContent={
+                  "\n Become a voice in your community and meet interesting people! \n \n"
+                }
+              />
+            </View>
+            <View
+              style={{
+                alignSelf: "center",
+                paddingTop: Dim.height * 0.1,
+                paddingBottom: Dim.height * 0.05,
+              }}
+            >
+              <CustomText p2 led style={{ textAlign: "center" }}>
+                email: nickopicz@gmail.com || phone: 908-410-7335
+              </CustomText>
+            </View>
+          </View>
+          <LinearGradient
+            style={{
+              width: Dim.width * 0.25,
+              height: Dim.height * 2,
+              opacity: 0.7,
+            }}
+            colors={[Colors.aegean, Colors.black]}
+            start={{ x: 1, y: 1 }}
+          >
+            <View
+              style={{
+                alignSelf: "center",
+                alignItems: "center",
+                paddingTop: Dim.height * 0.75,
+                paddingRight: 10,
+              }}
+            >
+              <LottieView
+                autoPlay
+                loop
+                source={require("../assets/down.json")}
+                style={{
+                  height: 200,
+                  width: 140,
+                }}
+              />
+            </View>
+          </LinearGradient>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
