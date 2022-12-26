c882778d44f2786253a92dcc3c302fb519bc43a5
Nick Ciraulo <nickopicz@gmail.com>
2022-12-26 14:02:14 -0500

    styles: added favicon

--- App.js
+++ App.js
@@ -9,12 +9,3 @@ export default function App() {
   });
   return <LandingPage />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
